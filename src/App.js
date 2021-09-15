c0fadcd48f06ce697f551cc27a4fe0005336fac0
ivil2021 <89508514326@mail.ru>
2021-09-15 18:11:37 +0300

    secind commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Hello12345</h1>
-        </div>
+    // return (
+    //     <div className="App">
+    //         <h1>Hello12345</h1>
+    //     </div>
+    // );
+
+    return React.createElement(
+        'div', // type of the element
+        null, // набор опций для нашего дива нулл или пустой объект {}
+        React.createElement('h1', null, 'hello world') // дети, вложенные в див
     );
 }
 
